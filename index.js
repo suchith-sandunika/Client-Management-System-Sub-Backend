41f87ebea5bf460a0468a65a04e2cfe9f17f5b0d
Suchith Sandunika <esuchith@gmail.com>
2024-12-11 16:19:16 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express";
+import con from './utils/db.js';
+import EmployeeRoutes from './Routes/EmployeeRotes.js';
+import AdminRoutes from './Routes/AdminRoutes.js';
+import cors from 'cors';
 
+const app =  express(); 
 
-const app =  express()
+app.use(express.json());
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Authorization']
+    }
+));
+app.use('/api/employees', EmployeeRoutes);
+app.use('/api/admin', AdminRoutes);
 
-app.listen(8800,()=>{
+app.listen(8800,() => {
     console.log("Connected to backend!")
-})
+});
