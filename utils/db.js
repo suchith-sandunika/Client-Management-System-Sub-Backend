219ad0e509fbc401969e8549eb80b555b37d69f3
Suchith Sandunika <esuchith@gmail.com>
2024-12-19 10:01:53 +0530

    updated backend code

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,9 @@
-import mysql from 'mysql2';
+import mysql from 'mysql';
 
 const con = mysql.createConnection({
     host: "localhost",
-    user:"suchith",
-    password:"+1234SSe",
+    user:"root",
+    password:"",
     database:"Client_Management_System"
 })
 
@@ -13,9 +13,28 @@ con.connect(function(err){
     } else{
         console.log("Connected")
     }
-}) 
+})
 
-export default con;
+export default con; 
+
+// import mysql from 'mysql2';
+
+// const con = mysql.createConnection({
+//     host: "localhost",
+//     user:"suchith",
+//     password:"+1234SSe",
+//     database:"Client_Management_System"
+// })
+
+// con.connect(function(err){
+//     if(err){
+//         console.log("connection error")
+//     } else{
+//         console.log("Connected")
+//     }
+// }) 
+
+// export default con;
 
 // import mysql from 'mysql2';
 
@@ -34,23 +53,4 @@ export default con;
 //     }
 // })
 
-// export default con;
-
-// import mysql from 'mysql';
-//
-// const con = mysql.createConnection({
-//     host: "localhost",
-//     user:"root",
-//     password:"",
-//     database:"Client_Management_System"
-// })
-//
-// con.connect(function(err){
-//     if(err){
-//         console.log("connection error")
-//     } else{
-//         console.log("Connected")
-//     }
-// })
-//
 // export default con;
